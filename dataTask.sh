7ffce23550a1fcee4a4b0197810bd629ed790fd4
yndben <yndben50@gmail.com>
2026-03-05 22:47:57 +0400

    feat: ajout fonction supprimer_base

--- dataTask.sh
+++ dataTask.sh
@@ -33,3 +33,18 @@ restaurer_base(){
 		echo "Aucun sauvegarde disponible"
 	fi
 }
+
+supprimer_base() {
+	if [ -f "tasks.txt" ]; then
+		read -p "Etes vous sure de vouloir supprimer la base? (y/n) : " confirmation
+		if [ "$confirmation" = "y" ]; then
+
+			rm tasks.txt
+			echo "La base a ete supprimer"
+		else 
+			echo "La supprimation a ete annuler" 
+		fi
+	else 
+		echo "La base n'existe pas, rien a ete supprimer"
+	fi
+}
